5fac748d50ac82de55eafc1470f87012378aabd0
emas93 <eduardamss93@gmail.com>
2026-09-01 04:44:52 -0300

    Commit inicial

---
+++ src/main/java/com/emas93/notificacao/NotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.emas93.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emas93/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emas93.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
